9d7c0b55db2a5e45d1fce515498027a4ce2646cd
CharanGhanta <charanghanta.1999@gmail.com>
2022-02-17 09:46:20 -0600

    added calculator

---
+++ Ghanta_Calculator/Ghanta_Calculator/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Ghanta_Calculator
+//
+//  Created by Ghanta,Sai Charan on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DisplayLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func AC(_ sender: Any) {
+    }
+    
+    
+    @IBAction func C(_ sender: Any) {
+    }
+    
+    
+    @IBAction func plusORminus(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Divide(_ sender: Any) {
+    }
+    
+    
+    @IBAction func multiply(_ sender: Any) {
+    }
+    
+    
+    @IBAction func subract(_ sender: Any) {
+    }
+    
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    
+    @IBAction func equalsto(_ sender: Any) {
+    }
+    
+    
+    @IBAction func percentage(_ sender: Any) {
+    }
+    
+    
+    @IBAction func decimal(_ sender: Any) {
+    }
+    
+    
+    @IBAction func zero(_ sender: Any) {
+    }
+    
+    
+    @IBAction func One(_ sender: Any) {
+    }
+    
+    
+    @IBAction func two(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Three(_ sender: Any) {
+    }
+    
+    
+    @IBAction func four(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func five(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func six(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func seven(_ sender: Any) {
+    }
+    
+    
+    @IBAction func eight(_ sender: Any) {
+    }
+    
+    
+    @IBAction func nine(_ sender: Any) {
+    }
+    
+    
+}
+
